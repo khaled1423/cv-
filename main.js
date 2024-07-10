ed09c3be30ca884d670955e05df052edc8b7187c
khaled1423 <khaeed1423@gmail.com>
2024-07-10 21:46:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let possitive = 'welcome';
+let massege = `${possitive} khaled`;
+console.log(massege);
+
+let age = 27
+
+if(age<26){
+
+    console.log('u can not assigne to this service');
+
+}
+else {
+    console.log('u will got 1000 monthly');
+}
+
+
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
